7022d9407baeae7de14d794e4cf9db59358bc018
Sharon Dansie <sharondansie@mail.weber.edu>
2017-03-16 16:26:00 +0000

    Initial Commit

---
+++ NorthCarolina_hw7_task2.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  NorthCarolina_hw7_task2.c
+ *       	Usage:  ./NorthCarolina_hw7_task2.c
+ *    Description:  Task2
+ *
+ *        Version:  1.0
+ *        Created:  03/16/2017 04:25:03 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Sharon Dansie (), sharondansie@weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+

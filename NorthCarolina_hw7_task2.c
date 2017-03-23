becd32c96829d226127d6d08199d2efc5b8280b4
Kurt Weloth <kurtweloth@mail.weber.edu>
2017-03-23 00:10:27 +0000

    First Go

--- NorthCarolina_hw7_task2.c
+++ NorthCarolina_hw7_task2.c
@@ -13,17 +13,51 @@
  */
 #include <stdio.h>		/* For Standard I/O */
 #include <stdlib.h>
+#define MAXMIN 10
+#include <time.h>
 
 /* Function Prototypes */
+void MaxMin(int numvals, int vals[], int* min, int* max);
 
 /* Main Program */
 int main(int argc, char *argv[])
 {
+	int i;
+	int list[MAXMIN];
+	int max = 0;
+	int min = 0;
 
+	srand(time(NULL));
+	list[0] = rand() % 100;
+	min = list[0];
+	printf("The integers of the array are:\n");
+
+	for (i = 0; i < MAXMIN; i++)
+	{
+		list[i] = rand() % 100;
+		printf("%d ", list[i]);
+	}
+	MaxMin(MAXMIN, list, &min, &max);
+	printf("\nThe max value of the array is: %d\n", max);
+	printf("The min value of the array is: %d\n", min);
 	return 0;
 }
 
-
 /* Function Defenitions */
-
+void MaxMin(int numvals, int vals[], int* min, int* max)
+{
+	int i;
+	for (i = 0; i < numvals; i++)
+	{
+		if(vals[i] > *max)
+		{
+			*max = vals[i];
+		}
+ 		if(vals[i] < *min)
+		{
+			*min = vals[i];
+		}
+	}
+	return; 
+}
 
